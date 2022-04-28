571733c7078d9fc9089032cca1ea827cef9e5259
MirzohidSaidakramov <mirzohidsaidakramov@gmail.com>
2022-04-28 19:21:36 +0500

    add

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('',(res,req)=>{
-    res.send('This is a a server that sends messages to a telegram channel/group using a telegram bot token')
-})
+router.get('/',(req, res)=>{
+    res.send('this is a a server that sends messages to a telegram channel/group using a telegram bot token');
+
+});
+
 module.exports = router;
